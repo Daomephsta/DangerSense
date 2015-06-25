bb21af85c81799f4b53217f32f2fefbbe96ef278
Leviathan143 <Leviathan14300@gmail.com>
2015-06-25 21:51:01 +1200

    Revert "Updated to 0.2.2.0"
    This reverts commit 7da55d529d85577e35e61272cad1e2e31a64c98f.

--- src/main/java/com/leviathan143/dangersense/DangerSenseMain.java
+++ src/main/java/com/leviathan143/dangersense/DangerSenseMain.java
@@ -1,22 +1,15 @@
 package com.leviathan143.dangersense;
 
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.WeightedRandomChestContent;
-import net.minecraftforge.common.ChestGenHooks;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.FMLCommonHandler;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 import com.leviathan143.dangersense.config.Config;
-import com.leviathan143.dangersense.handlers.DangerSenseHandler;
-import com.leviathan143.dangersense.items.DSItems;
-import com.leviathan143.dangersense.items.DSPotions;
 import com.leviathan143.dangersense.lib.DSConstants;
-import com.leviathan143.dangersense.recipes.DSRecipes;
-
-import cpw.mods.fml.common.FMLCommonHandler;
-import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.event.FMLPostInitializationEvent;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import com.leviathan143.dangersense.lib.DangerSenseHandler;
 
 @Mod(modid = DSConstants.MODID, name = DSConstants.MODNAME, version = DSConstants.VERSION)
 public class DangerSenseMain 
@@ -25,22 +18,13 @@ public class DangerSenseMain
 	public void preInit(FMLPreInitializationEvent event)
 	{
 		Config.preinit(event);
-		DSPotions.expandPotions();
-		DSPotions.init();
-		DSItems.init();
-		DSRecipes.init();
+		DangerSenseHandler.setGuiOpen();
 	}
 	
 	@Mod.EventHandler
 	public void Init(FMLInitializationEvent event)
 	{
-		ChestGenHooks.addItem(ChestGenHooks.DUNGEON_CHEST, new WeightedRandomChestContent(new ItemStack(DSItems.dangerPotion), 1, 1, 100));
-		ChestGenHooks.addItem(ChestGenHooks.MINESHAFT_CORRIDOR, new WeightedRandomChestContent(new ItemStack(DSItems.dangerPotion), 1, 1, 100));
-		ChestGenHooks.addItem(ChestGenHooks.PYRAMID_DESERT_CHEST, new WeightedRandomChestContent(new ItemStack(DSItems.dangerPotion), 1, 1, 100));
-		ChestGenHooks.addItem(ChestGenHooks.PYRAMID_JUNGLE_CHEST, new WeightedRandomChestContent(new ItemStack(DSItems.dangerPotion), 1, 1, 100));
-		ChestGenHooks.addItem(ChestGenHooks.STRONGHOLD_CORRIDOR, new WeightedRandomChestContent(new ItemStack(DSItems.dangerPotion), 1, 1, 100));
-		ChestGenHooks.addItem(ChestGenHooks.STRONGHOLD_CROSSING, new WeightedRandomChestContent(new ItemStack(DSItems.dangerPotion), 1, 1, 100));
-		ChestGenHooks.addItem(ChestGenHooks.STRONGHOLD_LIBRARY, new WeightedRandomChestContent(new ItemStack(DSItems.dangerPotion), 1, 1, 100));
+		
 	}
 	
 	@Mod.EventHandler
--- src/main/java/com/leviathan143/dangersense/config/Config.java
+++ src/main/java/com/leviathan143/dangersense/config/Config.java
@@ -1,23 +1,19 @@
 package com.leviathan143.dangersense.config;
 
 import net.minecraftforge.common.config.Configuration;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 public class Config 
 {
 	private static Configuration config;
 	public static float detectionRange;
 	public static int scanTimeInTicks;
-	public static int potionID;
-	public static boolean easyRecipe;
 	public static void preinit(FMLPreInitializationEvent event)
 	{
 		config = new Configuration(event.getSuggestedConfigurationFile());
 		config.load();
 		detectionRange = config.getFloat("detectionRange", Configuration.CATEGORY_GENERAL, 5, 3, 10, "How near to the player a hostile mob must be to activate the danger sense");
 		scanTimeInTicks = config.getInt("scanTimeInTicks", Configuration.CATEGORY_GENERAL, 20, 10, 100, "The number of ticks that will pass between checks for hostile mobs");
-		potionID = config.getInt("potionID", Configuration.CATEGORY_GENERAL, 42, 32, 256, "The id of the Dangersense Potion");
-		easyRecipe = config.getBoolean("easyRecipe", Configuration.CATEGORY_GENERAL, false, "Enables the easy recipe for the Dangersense Potion");
 		config.save();
 	}
 }
