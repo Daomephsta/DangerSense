fc913b09a6267f00979d769fccd8341da0e7357b
Leviathan143 <Leviathan143@users.noreply.github.com>
2015-07-09 22:05:56 +1200

    Update DangerSenseHandler.java

--- src/main/java/com/leviathan143/dangersense/handlers/DangerSenseHandler.java
+++ src/main/java/com/leviathan143/dangersense/handlers/DangerSenseHandler.java
@@ -101,24 +101,7 @@ public class DangerSenseHandler
         	int l = scaledresolution.getScaledHeight();
 			if (event.phase == Phase.END && hostileNearby)
 			{	
-	        	GL11.glDepthMask(false);
-	        	GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-	        	GL11.glEnable(GL11.GL_BLEND);
-	        	OpenGlHelper.glBlendFunc(770, 771, 1, 0);
-	        	GL11.glDisable(GL11.GL_ALPHA_TEST);
-	        	mc.getTextureManager().bindTexture(RES_DANGER_OVERLAY);
-	        	Tessellator tessellator = Tessellator.instance;
-	        	tessellator.startDrawingQuads();
-	        	tessellator.addVertexWithUV(0.0D, (double)l, -90.0D, 0.0D, 1.0D);
-	        	tessellator.addVertexWithUV((double)k, (double)l, -90.0D, 1.0D, 1.0D);
-	        	tessellator.addVertexWithUV((double)k, 0.0D, -90.0D, 1.0D, 0.0D);
-	        	tessellator.addVertexWithUV(0.0D, 0.0D, -90.0D, 0.0D, 0.0D);
-	        	tessellator.draw();
-	        	GL11.glDepthMask(true);
-	        	GL11.glEnable(GL11.GL_ALPHA_TEST);
-	        	GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-	        	GL11.glDisable(GL11.GL_BLEND);
-	        
+	        	
 			}
 		//}
 		if (blindSpot == 7)
